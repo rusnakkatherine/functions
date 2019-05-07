d4647c0fe414249f7f3069c38ff6c87532c0716a
katherine <rusnakkat@gmail.com>
2019-05-06 20:35:37 -0700

    hoisting

---
+++ js/hoisting.js
@@ -0,0 +1,9 @@
+funcDeclaration();
+function funcDeclaration(){
+    console.log('i\'m inside a function declaration');
+
+}
+funcExpression();
+var funcExpression = function (){
+    console.log('i\'m inside a function expression');
+};
